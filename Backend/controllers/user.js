34ea3459f6ea522fc7c31ec7ff1adca26c8a4f82
krishpuw <144583732+krishpuw@users.noreply.github.com>
2024-07-28 04:09:04 +0400

    as of saturday stuff;

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -50,6 +50,12 @@ export const Login = async(req,res)=>{
     }
 }
 
+export const Logout = async (req,res) => {
+    return res.status(200).cookie("token", "", {expiresIn:new Date(Date.now()), httpOnly:true}).json({
+        message:"User logged out successfully.",
+        success:true,
+    });
+}
 
 export  const Register = async (req, res) => {
     try {
--- Backend/route/userRoute.js
+++ Backend/route/userRoute.js
@@ -10,11 +10,12 @@
 // export default router;
 
 import express from "express";
-import { Login,Register } from "../controllers/user.js";
+import { Login,Logout,Register } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.route("/register").post(Register);
 router.route("/login").post(Login);
+router.route("/logout").get(Logout);
 
 export default router;
